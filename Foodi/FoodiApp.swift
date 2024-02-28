671f51150805c50cec96a29b7be28e946f72acad
bradensmith <braden@appsmiths.io>
2024-02-28 16:40:01 -0600

    Initial Commit

---
+++ Foodi/FoodiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiApp.swift
+//  Foodi
+//
+//  Created by Braden Smith on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
