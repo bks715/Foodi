a0f1ff052bc522150eecbad52eff9f8c5b9f8f34
bradensmith <braden@appsmiths.io>
2024-02-29 11:04:14 -0600

    Added an app delegate and code to initialize and register our custom fonts from my appsmiths essential package.

--- Foodi/FoodiApp.swift
+++ Foodi/FoodiApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import AppsmithsEssentials
 
 @main
 struct FoodiApp: App {
+    
+    //Connect the App Delegate to our SwiftUI lifecycle
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             RecipeListView()
         }
     }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        //Initialize the Appsmiths Font Package
+        self.registerFonts()
+        
+        return true
+    }
+    
+    ///This function initializes and registers the fonts included in my custom Appsmiths Package.
+    private func registerFonts(){
+        //Run in Do-Catch Block to handle errors
+        do{
+            try AppsmithsEssentials.registerFonts()
+        }catch{
+            //If the fonts don't register print the error
+            print(error)
+        }
+    }
+    
 }
